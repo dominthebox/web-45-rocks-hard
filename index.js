c00f83c3b4c2855cb7131ddab3f2e245b3f922f8
dominthebox <dsallustro@gmail.com>
2021-09-02 13:08:20 -0400

    wip, done for now

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const server = require('express')()
+require('dotenv').config() // this allows to stash "artificial env variables" in a file
+const express = require('express')
+const cors = require('cors')
+const server = express()
 
-server.listen(5000, () => {
-    console.log('listening on port 5000')
+console.log(process.env.USER)
+console.log(process.env.API_SECRET)
+console.log(process.env.LADY)
+
+const PORT = process.env.PORT || 5000 // fallback is nice
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+    res.send('<h1>Web 45 ROCKS!</h1>')
+})
+
+server.get('/api', (req, res) => {
+    res.json({ message: 'Web 45 Rocks!' })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })
