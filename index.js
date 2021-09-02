fdfc37c820c60a62d6cc26647a1a56153e74e591
dominthebox <dsallustro@gmail.com>
2021-09-02 13:59:57 -0400

    added react app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config() // this allows to stash "artificial env variables" in a file
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const server = express()
@@ -11,13 +12,15 @@ const PORT = process.env.PORT || 5000 // fallback is nice
 
 server.use(express.json())
 server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
-server.get('/', (req, res) => {
-    res.send('<h1>Web 45 ROCKS!</h1>')
-})
 
 server.get('/api', (req, res) => {
     res.json({ message: 'Web 45 Rocks!' })
+}
+)
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 server.listen(PORT, () => {
